179bbfde8039bb09fc033456e555e69a22b160a0
CarlosRenan01 <107955803+CarlosRenan01@users.noreply.github.com>
2023-04-27 20:50:41 -0300

    Add files via upload
    CRUD feito em Node.js.

---
+++ data-base.js
@@ -0,0 +1,11 @@
+ const users = [
+    {id:1, name:"Carlos Renan", email:"carlosrenan@hotmail.com"},
+    
+    {id:2, name:"Evelyn Souza", email:"evelynsouza@hotmail.com"},
+
+    {id:3, name:"Matteo Ferreira", email:"matteof@hotmail.com"},
+
+    {id:4, name:"Cecilia Ferreira", email:"ceciliaf@hotmail.com"}
+];
+
+module.exports = users
--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const users = require('./data-base');
 
+const app = express();
+const port = 6500;
+
+app.use(bodyParser.json());
+
+
+//Rota GET para exibir todos os usuarios
+app.get("/users", (req, res) => { 
+    res.send(users)
+})
+//Rota POST para criar um novo usuario 
+app.post("/users", (req, res) => { 
+    const user = { id: req.body.id , name: req.body.name , email: req.body.email }
+    users.push(user)
+    res.send(users)
+})
+//Rota PUT para atulizar um usuario existente
+app.put("/users/:id", (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    user.name = req.body.name;
+    user.email = req.body.email;
+    res.send(user)
+})
+//Rota DELETE para excluir um usuario existente
+app.delete("/users/:id", (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id))
+    const index = users.indexOf(user);
+    users.splice(index, 1);
+    res.send(users)
+})
+
+app.listen(port, () => {console.log("Servidor rodando na porta " + port)})
